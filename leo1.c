329d36ce6988b1624b71022e785352012ed14bd9
leoinfant <leoinfant62@gmail.com>
2017-04-19 15:05:54 +0530

    Add files via upload

---
+++ leo1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int n;
+  scanf("%d",&n);
+  if(n>0)
+    prinf("positive");
+  else if(n<0)
+    printf("negative");
+  else
+  printf("zero");
+}
