b5c5268d1b7acecf64186739935390745cd6c074
leoinfant <leoinfant@users.noreply.github.com>
2017-07-14 12:48:15 +0530

    Create seed.c

---
+++ seed.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int a,b,t,x1=1;
+	scanf("%d %d",&a,&b);
+	t=a;
+	int i;
+	while(x!=0)
+	{
+		x1*=a%10;
+	//	printf("%d",x1);
+		a=a/10;
+	}
+	if((x1*t)==b)
+		printf("seed");
+	else
+		printf("not seed");
+	return 0;
+}
